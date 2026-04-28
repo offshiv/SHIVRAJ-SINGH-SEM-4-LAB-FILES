45172a9247d5fe649ab1b953b1e7a804ecca6d4e
offshiv <shivrajsingh4480@gmail.com>
2026-04-29 03:57:52 +0530

    Update cycle_detection.cpp

--- Lab6/cycle_detection.cpp
+++ Lab6/cycle_detection.cpp
@@ -2,12 +2,12 @@
 #include<vector>
 using namespace std;
 
-bool dfs(int u, vector<int> adj[], bool visited[], bool recStack[]){
+bool dfs(int u, const vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& recStack){
     visited[u]=true;
     recStack[u]=true;
-    for(int v: adj[u]){
-        if(!visited[v] && dfs(v, adj, visited, recStack)) return true;
-        else if(recStack[v]) return true;
+    for(int nei: adj[u]){
+        if(!visited[nei] && dfs(nei, adj, visited, recStack)) return true;
+        else if(recStack[nei]) return true;
     }
     recStack[u]=false;
     return false;
@@ -17,7 +17,7 @@ int main(){
     int v, e;
     cout<<"Enter vertices and edges: ";
     cin>>v>>e;
-    vector<int> adj[v];
+    vector<vector<int>> adj(v);
     for(int i=0; i<e; i++){
         int u, w; cin>>u>>w;
         adj[u].push_back(w);
